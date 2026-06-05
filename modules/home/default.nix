bd2148997c2dd525d69c8ab56849113013a7025a
MasterEnderman <xxmr.endermanxx@gmail.com>
2026-06-05 11:20:35 +0200

    Update default.nix

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -4,17 +4,7 @@
       username      = "enderman";
       homeDirectory = "/home/enderman";
       stateVersion  = "25.05";
-      packages      = with pkgs; [ foot fastfetch ];
     };
-
-    programs.noctalia-shell = {
-      enable   = true;
-      settings = {
-        colorSchemes.predefinedScheme = "Noctalia (default)";
-        general.telemetryEnabled      = false;
-      };
-    };
-
     programs.home-manager.enable = true;
   };
 }
