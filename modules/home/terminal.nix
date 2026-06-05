e74a73284ebfa813bdbe5a3025888b16e7488d90
MasterEnderman <xxmr.endermanxx@gmail.com>
2026-06-05 11:24:08 +0200

    Create terminal.nix

---
+++ modules/home/terminal.nix
@@ -0,0 +1,16 @@
+{ ... }: {
+  flake.homeModules.enderman = { pkgs, lib, ... }: {
+    home.packages = with pkgs; [ fastfetch pfetch-rs ];
+
+    home.sessionVariables.TERMINAL = "foot";
+
+    programs.foot = {
+      enable   = true;
+      settings.main.font = "monospace:size=11";
+    };
+
+    programs.bash.bashrcExtra = ''
+      pfetch
+    '';
+  };
+}
