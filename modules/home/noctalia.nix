1ce14eaebb5d56085a3e0881853e4ed7c392063c
MasterEnderman <xxmr.endermanxx@gmail.com>
2026-06-05 11:19:42 +0200

    Update noctalia.nix

--- modules/home/noctalia.nix
+++ modules/home/noctalia.nix
@@ -1,1 +1,11 @@
-
+{ ... }: {
+  flake.homeModules.enderman = { ... }: {
+    programs.noctalia-shell = {
+      enable   = true;
+      settings = {
+        colorSchemes.predefinedScheme = "Noctalia (default)";
+        general.telemetryEnabled      = false;
+      };
+    };
+  };
+}
