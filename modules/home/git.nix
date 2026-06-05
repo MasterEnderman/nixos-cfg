9c69a4346398176069c943064f8159e2aae7a093
MasterEnderman <xxmr.endermanxx@gmail.com>
2026-06-05 11:18:42 +0200

    Create git.nix

---
+++ modules/home/git.nix
@@ -0,0 +1,9 @@
+{ ... }: {
+  flake.homeModules.enderman = { ... }: {
+    programs.git = {
+      enable    = true;
+      userName  = "MasterEnderman";
+      userEmail = "xxmr.endermanxx@gmail.com";
+    };
+  };
+}
