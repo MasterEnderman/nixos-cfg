9729ea4a14ddd7a01691115fcc66bdde7567fbdd
MasterEnderman <xxmr.endermanxx@gmail.com>
2026-06-23 13:00:04 +0200

    this should already be managed by disko.nix

--- modules/hosts/hp-probook/default.nix
+++ modules/hosts/hp-probook/default.nix
@@ -20,11 +20,6 @@
         fileSystems."/nix".neededForBoot = true;
         fileSystems."/persist".neededForBoot = true;
 
-        boot.initrd.luks.devices."cryptroot" = {
-          device = "/dev/disk/by-partlabel/disk-main-luks";
-          allowDiscards = true;
-        };
-
         my.nixos.impermanence.enable = true;
 
         my.nixos.kernel = {
