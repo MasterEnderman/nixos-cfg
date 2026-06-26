17730a224b601e0a48c4fe19f5a46e522990fa0d
MasterEnderman <xxmr.endermanxx@gmail.com>
2026-06-26 11:48:44 +0200

    maybe fix noctalia auto start

--- modules/home/enderman/niri.nix
+++ modules/home/enderman/niri.nix
@@ -11,7 +11,7 @@
   programs.niri.settings = {
     # Spawn noctalia-shell at startup
     spawn-at-startup = [
-      {command = ["noctalia-shell"];}
+      {argv = ["${pkgs.noctalia}/bin/noctalia"];}
     ];
 
     # Keyboard Layout
@@ -20,6 +20,35 @@
 
     # Keybinds
     binds = {
+      # --- navigation and window management ---
+      "Mod+H".action.focus-column-left = {};
+      "Mod+J".action.focus-window-down = {};
+      "Mod+K".action.focus-window-up = {};
+      "Mod+L".action.focus-column-right = {};
+
+      "Mod+Left".action.focus-column-left = {};
+      "Mod+Down".action.focus-window-down = {};
+      "Mod+Up".action.focus-window-up = {};
+      "Mod+Right".action.focus-column-right = {};
+
+      "Mod+Tab".action.focus-window-down = {};
+      "Mod+Q".action.close-window = {};
+      "Mod+F".action.fullscreen-window = {};
+      "Mod+M".action.maximize-column = {};
+
+      "Mod+Comma".action.focus-workspace-up = {};
+      "Mod+Period".action.focus-workspace-down = {};
+
+      "Mod+1".action.focus-workspace = 1;
+      "Mod+2".action.focus-workspace = 2;
+      "Mod+3".action.focus-workspace = 3;
+      "Mod+4".action.focus-workspace = 4;
+      "Mod+5".action.focus-workspace = 5;
+      "Mod+6".action.focus-workspace = 6;
+      "Mod+7".action.focus-workspace = 7;
+      "Mod+8".action.focus-workspace = 8;
+      "Mod+9".action.focus-workspace = 9;
+
       # Terminal
       "Mod+Return".action.spawn = ["${lib.getExe pkgs.foot}"];
 
